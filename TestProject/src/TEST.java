ff2b8c7de5f0c4f270a284564704fa38e4165dc1
COG34LP <COG34LP@DESKTOP-QA3RI4M>
2022-09-19 00:14:27 -0400

    ccc

--- TestProject/src/TEST.java
+++ TestProject/src/TEST.java
@@ -3,7 +3,10 @@ public class TEST {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println();
+     System.out.println();
+     for (int i = 0; i < 10; i++) {
+		System.out.println("Hello world");
+	}
 	}
 
 }
