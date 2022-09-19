f149bb4a9b6088eca51274b582cf90d5a5bb6f71
COG34LP <COG34LP@DESKTOP-QA3RI4M>
2022-09-18 23:42:21 -0400

    First Commit

---
+++ Loops_ControlStatements/src/com/app/Q1.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		int num1 = 5; 
+        int num2 = 6;
+        if ((num2 = 1) == num1)
+            System.out.print(num2);
+        else 
+            System.out.print(++num2);
+
+	}
+
+}
---
+++ Loops_ControlStatements/src/com/app/Q3.java
@@ -0,0 +1,12 @@
+package com.app;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		for (i = 0; i < 10; i++) ;
+		System.out.println(i);
+
+	}
+
+}
